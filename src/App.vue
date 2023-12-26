049f8d585fe2c94aefae25579f07900baa887312
Regina Alyssa <reginaalyssa01809@gmail.com>
2023-12-27 03:19:17 +0800

    Update contents with header and add basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,14 @@
 <template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia Logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
 </template>
+
+<script>
+  export default {
+
+  }
+</script>
