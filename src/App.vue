f6110647bb4eebbc5d97b15acb9ab96f55989ed4
Regina Alyssa <reginaalyssa01809@gmail.com>
2023-12-27 05:40:25 +0800

    Display each task using temporary title text

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <img src="./assets/pinia-logo.svg" alt="Pinia Logo" />
       <h1>Pinia Tasks</h1>
     </header>
+    <div class="task-list">
+      <div v-for="task in tasksStore.tasks">
+        <p>{{ task.title }}</p>
+      </div>
+    </div>
   </main>
 </template>
 
