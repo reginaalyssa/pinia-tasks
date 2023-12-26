24ed9100739834462ac76397f88023850b37376c
Regina Alyssa <reginaalyssa01809@gmail.com>
2023-12-27 05:31:02 +0800

    Fix tasks store to use state

--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia'
 
 export const useTasksStore = defineStore('tasks', {
-  store: () => ({
+  state: () => ({
     tasks: [
       { id: 1, title: "Buy milk", isFave: false },
       { id: 2, title: "Play Monopoly Deal", isFave: true }
-    ]
+    ],
+    name: "Task Store"
   })
 })
