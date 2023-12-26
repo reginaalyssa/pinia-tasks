25c4308e342a7c228743bea68c9adafb4a5d9960
Regina Alyssa <reginaalyssa01809@gmail.com>
2023-12-27 07:07:24 +0800

    Implement filters to toggle between showing all tasks and favorite tasks

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,17 @@
       <img src="./assets/pinia-logo.svg" alt="Pinia Logo" />
       <h1>Pinia Tasks</h1>
     </header>
-    <div class="task-list">
+    <nav class="filter">
+      <button @click="filter = 'All'">All Tasks</button>
+      <button @click="filter = 'Faves'">Fave Tasks</button>
+    </nav>
+    <div class="task-list" v-if="filter === 'All'">
       <p>All Tasks</p>
       <div v-for="task in tasksStore.tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'Faves'">
       <p>Fave Tasks</p>
       <div v-for="task in tasksStore.faves">
         <TaskDetails :task="task" />
@@ -22,12 +26,15 @@
 <script>
   import TaskDetails from '@/components/TaskDetails.vue'
   import { useTasksStore } from '@/stores/tasks'
+  import { ref } from 'vue'
 
   export default {
     setup() {
       const tasksStore = useTasksStore()
 
-      return { tasksStore }
+      const filter = ref("All")
+
+      return { tasksStore, filter }
     },
     components: {
       TaskDetails
