3d0fceb7e598c9a65c83b61a5a268fe72e5e028f
Regina Alyssa <reginaalyssa01809@gmail.com>
2023-12-27 08:48:24 +0800

    Implement deleting of task and toggling task as fave

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,14 +2,31 @@
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons">delete</i>
-      <i class="material-icons">favorite</i>
+      <i
+        class="material-icons"
+        @click="tasksStore.deleteTask(task.id)"
+      >
+        delete
+      </i>
+      <i
+        class="material-icons"
+        @click="tasksStore.toggleFave(task.id)"
+      >
+        favorite
+      </i>
     </div>
   </div>
 </template>
 
 <script>
+  import { useTasksStore } from '@/stores/tasks'
+
   export default {
-    props: ['task']
+    props: ['task'],
+    setup() {
+      const tasksStore = useTasksStore()
+
+      return { tasksStore }
+    }
   }
 </script>
